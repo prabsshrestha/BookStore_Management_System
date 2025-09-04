68c721aeac7cfd86e03aacbe62c0c2f10b48d340
Prabsshrestha <prabs.shrestha@gmail.com>
2025-09-04 23:48:59 +0545

    fix

--- Bulky.Utility/ApplicationUserClaimsPrincipalFactory.cs
+++ Bulky.Utility/ApplicationUserClaimsPrincipalFactory.cs
@@ -23,6 +23,12 @@ public class ApplicationUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<
         var name = (user as ApplicationUser)?.Name ?? "Guest";
         identity.AddClaim(new Claim("Name", name));
 
+        var roles = await _userManager.GetRolesAsync(user);
+        foreach (var role in roles)
+        {
+            identity.AddClaim(new Claim(ClaimTypes.Role, role));
+        }
+
         return identity;
     }
 }
